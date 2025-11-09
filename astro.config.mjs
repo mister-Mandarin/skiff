690dce971da6194119ad48114509d31e79c8c7d8
Mandarin <andr.poluehktov@yandex.ru>
2025-11-09 03:12:13 +0300

    30%

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,9 @@
 import { defineConfig } from 'astro/config';
 import { webcore } from 'webcoreui/integration'
 
+import purgecss from 'astro-purgecss';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [webcore()],
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `
-                        @use 'webcoreui/styles' as *;
-                        @use 'webcoreui/config' as *;
-                    `
-                }
-            }
-        }
-    }
+    integrations: [webcore(), purgecss()],
 });
